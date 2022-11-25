55c7edaf660396396566eaa098d85c449eb48d71
Eileen Yoon <eilny@proton.me>
2022-11-26 04:50:09 +0900

    ane: matmul2d user input

--- proxyclient/experiments/ane_matmul2d.py
+++ proxyclient/experiments/ane_matmul2d.py
@@ -6,28 +6,14 @@ sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
 from m1n1.setup import *
 from m1n1.shell import run_shell
 
+import numpy as np
+
 from m1n1.ane import ANE
 from m1n1.ane.ane_tm import TaskManager
 from m1n1.ane.ane_context import TaskSequence, ReqManager
 from m1n1.ane.ane_utils import zero_pad, make_padding
 from m1n1.ane.compiler.matmul2d import compile_matmul2d
 
-import numpy as np
-
-# userspace
-
-M = 5 # HOLD
-N = 8 # 1 <= N <= 500
-P = 3 # HOLD
-src1_arr = np.arange(M*N).reshape((M, N)).astype(np.float64)
-src2_arr = np.arange(N*P).reshape((N, P)).astype(np.float64)
-
-ts_buf = compile_matmul2d(N)
-# open('compiled.bin', 'wb').write(ts_buf)
-ts_prop = [0x274, 0x274, 0x278, 0x278, 0x274] # TODO
-ts = TaskSequence(ts_buf, ts_prop)
-
-# ----------------------------------
 
 # init platform
 ane = ANE(u)
@@ -46,6 +32,7 @@ tm = TaskManager(ane, 0x26bc00000) # TODO other boards ?
 tm.init_tqs()
 
 # ----------------------------------
+
 reqmngr = ReqManager(ane)
 reqmngr.init_req(ts)
 
@@ -68,51 +55,55 @@ def prep_bufs(src1_arr, src2_arr):
 
 prep_bufs(src1_arr, src2_arr)
 reqmngr.make_fifo_head() # push fifo
-# ane.bufmngr.dump_map()
-# ane.bufmngr.dump_bufs()
 
-# ----------------------------------
 
-# lets goooo
 def push2hw(req):
-    print('\n\npushing to hw...')
+    print('pushing to hw...')
     (dma_r1, dma_w1, dma_rw1) = ane.get_dma_perf_stats()
-
-    dst_iova = req.bar.dst
-    dst_buf_prev = ane.ioread(dst_iova, ane.TILE_SIZE)
-    # open("prev.bin", "wb").write(dst_buf_prev)
     ane.get_timestamp()
 
-    # these two are the actual pushes
-    # everything else here is just sugar
     tm.enqueue_tq(req)
     tm.execute_tq(req)
 
     ane.get_timestamp()
-    dst_buf_post = ane.ioread(dst_iova, ane.TILE_SIZE) # diff :)
-    # open("post.bin", "wb").write(dst_buf_post)
-
     (dma_r2, dma_w2, dma_rw2) = ane.get_dma_perf_stats()
     print('perf: total: dma_r: 0x%x, dma_w: 0x%x, dma_rw: 0x%x' 
                                         % (dma_r2, dma_w2, dma_rw2))
     print('perf: delta: dma_r: 0x%x, dma_w: 0x%x, dma_rw: 0x%x' 
                                         % (dma_r2-dma_r1, dma_w2-dma_w1, 
                                            dma_rw2-dma_rw1))
-    
+
+    dst_buf = ane.ioread(req.bar.dst, ane.TILE_SIZE)
+    return dst_buf
+
+
+def main(src1_arr, src2_arr, N, dst_shape):
+    ts_buf = compile_matmul2d(N)
+    ts_prop = [0x274, 0x274, 0x278, 0x278, 0x274] # TODO
+    ts = TaskSequence(ts_buf, ts_prop)
+
+    reqmngr.init_req(ts)
+    prep_bufs(src_arr, krn_arr)
+    reqmngr.make_fifo_head() # push fifo
+    dst_buf = push2hw(reqmngr.req)
+
     ref_arr = src1_arr @ src2_arr
     print('ref_arr: \n')
     print(ref_arr, '\n\n')
     print('drumroll pls .....')
-    dst_arr = ane.tiler.tile2arr(dst_buf_post, (M, P))
+
+    dst_arr = ane.tiler.tile2arr(dst_buf, dst_shape)
     print('dst_arr: \n')
     print(dst_arr, '\n\n')
-
-    print('shape eq: %r' % (dst_arr.shape == ref_arr.shape))
-    print('arr eq: %r' % (np.array_equal(dst_arr, ref_arr)))
-    print('\n')
     return dst_arr
 
-# push2hw(reqmngr.req)
+
+M = 5 # HOLD
+N = 8 # 1 <= N <= 500
+P = 3 # HOLD
+src1_arr = np.arange(M * N).reshape((M, N)).astype(np.float64)
+src2_arr = np.arange(N * P).reshape((N, P)).astype(np.float64)
+main(src1_arr, src2_arr, N, (M, P))
 
 
 run_shell(globals(), msg="Have fun!")
